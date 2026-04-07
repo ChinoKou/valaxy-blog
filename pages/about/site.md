edb29b9624580689c57c4e8a3276d61576600630
ChinoKou <ChinoKou@outlook.com>
2026-04-07 13:43:20 +0800

    init

---
+++ pages/about/site.md
@@ -0,0 +1,14 @@
+---
+title: 关于站点
+---
+
+## 网站概览
+
+这是一个使用 **[Valaxy](https://github.com/YunYouJun/valaxy)** 框架构建的静态博客站点。
+
+## 特点
+
+- ⚡ 快速、轻量级的网站生成
+- 📝 简洁优雅的设计
+- 🚀 优良的网页性能
+- 🌍 通过 [ESA](https://www.aliyun.com/product/esa) / [EO](https://cloud.tencent.com/product/teo) 全球加速
---
+++ valaxy.config.ts
@@ -0,0 +1,59 @@
+import type { UserThemeConfig } from "valaxy-theme-yun";
+import { defineValaxyConfig } from "valaxy";
+
+// add icons what you will need
+const safelist = ["i-ri-home-line"];
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: "yun",
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: "ChinoKou的小站",
+    },
+
+    fireworks: {
+      enable: true,
+      colors: ["#FECCFF"],
+    },
+
+    pages: [
+      // {
+      //   name: "我的小伙伴们",
+      //   url: "/links/",
+      //   icon: "i-ri-genderless-line",
+      //   color: "dodgerblue",
+      // },
+      // {
+      //   name: "喜欢的女孩子",
+      //   url: "/girls/",
+      //   icon: "i-ri-women-line",
+      //   color: "hotpink",
+      // },
+    ],
+
+    footer: {
+      since: 2026,
+      cloud: {
+        enable: true,
+      },
+      powered: false,
+      icon: {
+        enable: false,
+      },
+      beian: {
+        enable: true,
+        icp: "粤ICP备2026033775号-1",
+        police: "",
+      },
+    },
+  },
+
+  unocss: { safelist },
+});
